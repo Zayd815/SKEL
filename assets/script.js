2d6241e4014beef361a40c7f6dbc3a06225d2f8d
Zayd815 <129797953+Zayd815@users.noreply.github.com>
2024-10-29 20:52:14 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector(".trusted-carousel");
-    const items = carousel.innerHTML; // Clone le contenu du carousel
-
-    // Ajoute le clone du contenu pour un effet cyclique
-    carousel.innerHTML += items; 
-
-    // Applique un défilement infini en ajustant l'animation
-    let scrollAmount = 0;
-    setInterval(() => {
-        scrollAmount += 1;
-        if (scrollAmount >= carousel.scrollWidth / 2) {
-            scrollAmount = 0;
-        }
-        carousel.scrollTo(scrollAmount, 0);
-    }, 20);
-});
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
