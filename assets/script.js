9c0cd0e68fbf55aa781f332b217d996544d208e0
Zayd815 <129797953+Zayd815@users.noreply.github.com>
2024-10-31 03:37:46 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('show');
 }
+
+document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+        const navLinks = document.querySelector('.nav-links');
+        if (navLinks.classList.contains('show')) {
+            navLinks.classList.remove('show');
+        }
+    });
+});
