bf8bb809546d01804b5c365319aeb219458f3654
Zayd815 <129797953+Zayd815@users.noreply.github.com>
2024-10-31 07:19:00 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,19 @@ document.querySelectorAll('.nav-links li a').forEach(link => {
         }
     });
 });
+
+window.addEventListener('scroll', function() {
+    const cta = document.getElementById('cta');
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const ctaDiscover = document.querySelector('#accueil button');
+
+    // Obtenez la position de défilement de la page et la position du CTA "Découvrir nos services"
+    const ctaDiscoverPosition = ctaDiscover.getBoundingClientRect().bottom;
+
+    // Vérifiez si le header recouvre totalement le CTA "Découvrir nos services"
+    if (ctaDiscoverPosition <= headerHeight) {
+        cta.classList.remove('hidden');
+    } else {
+        cta.classList.add('hidden');
+    }
+});
